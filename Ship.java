dacbbc75daacde0f8966533ece0e166cd39838d1
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-13 14:42:22 +0100

    Add isSunk method to Ship

--- Ship.java
+++ Ship.java
@@ -2,15 +2,26 @@ import java.util.ArrayList;
 
 public class Ship {
     
-    //private int 
+    //private int
+    private ArrayList<Square> listSquareOfShip; 
     
     public Ship(ArrayList<Square> arraySquareNewShip, ArrayList<Square> arrayFieldsReserved){
         for(Square square : arraySquareNewShip){
             square.setShip();
+            listSquareOfShip.add(square);
         }
 
         for(Square square : arrayFieldsReserved){
             square.setReserved();
         }
+    }
+
+    public boolean isSunk(){
+        for(Square square : listSquareOfShip){
+            if(!square.getIsChosen()){
+                return false;
+            }
+        }
+        return true;
     }
 }
