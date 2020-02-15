4f6e6eefb97e822eee87882c937dc20c1c4e612f
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-15 20:07:11 +0100

    Add method inputFromUser

--- View.java
+++ View.java
@@ -1,4 +1,6 @@
-public class View{
+import java.util.Scanner;
+
+public class View {
     
     public View(){ 
         // nie wiem czy coś tu w ogóle musi być
@@ -7,6 +9,14 @@ public class View{
     public void printOcean(Ocean currentOcean, Game currentGame) {
         // tu będzie można dodać drukowanie nagłówka, literek ABC... etc.
         System.out.print(currentOcean.toString(currentGame));
+    }
+
+    public String inputFromUser(String textToView){
+        System.out.println(textToView + ":");
+        Scanner scannerFromUser = new Scanner(System.in);
+        String input = scannerFromUser.nextLine();
+
+        return input;
     }
 
     public void clearScreen() {  
