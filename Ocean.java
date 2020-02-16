e91755b0e4bbde1ce0c7e86b76cea057352e21cd
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-02-16 21:29:41 +0100

    implement method shoot in Ocean

--- Ocean.java
+++ Ocean.java
@@ -4,6 +4,7 @@ public class Ocean{
 
     ArrayList<ArrayList<Square>> board;
     ArrayList<Ship> shipsArray;
+    Ship ship;
 
     final static int WIDTH = 10;
     final static int HEIGHT = 10;
@@ -21,6 +22,15 @@ public class Ocean{
             this.board.add(row);
         }
 
+    }
+
+    public boolean shoot(int x, int y){
+        for(Ship ship: shipsArray){
+            if(ship.isShooted(board.get(y).get(x))){
+                return true;
+            }
+        }
+        return false;
     }
 
     // public void printOcean(){
